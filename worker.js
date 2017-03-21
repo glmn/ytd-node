f2ac0fbce3d8143d84dcbae8092ac3314fb204bb
Stanislav Gelman <speedster.ggg@gmail.com>
2017-03-21 17:21:12 +0300

    Moved Accounts class to his own file

--- worker.js
+++ worker.js
@@ -15,6 +15,8 @@ var junk = require('junk');
 var opn = require("opn");
 var fs = require("fs");
 var sqlite = require('sqlite3').verbose();
+var Accounts = require('./accounts.js');
+var accounts = new Accounts();
 
 require('dotenv').config();
 	
@@ -310,61 +312,25 @@ class Worker {
 
 }
 
-class Accounts {
 
-	constructor()
-	{
-		this.db = new sqlite.Database('accounts.db', sqlite.OPEN_READWRITE);
-		this.currentIndex = 0
-		this.current = {}
-	}
+/*
+	Init Accounts class and connection to accounts database
+ */
 
-	fetchAll()
-	{
-		return new Promise((resolve,reject) => {
-			this.db.all('SELECT * FROM accounts', (err, rows) => {
-				if(err) reject(err)
-				resolve(rows)
-			})
-		})
-	}
-
-	count()
-	{
-		return this.list.length;
-	}
-
-	showList()
-	{
-		debug.log(this.list);
-	}
-	
-	select(index)
-	{
-		return new Promise((resolve,reject) => {
-			if(this.count == 0) reject(new Error('Zero accounts in database'))
-			if(this.count-1 > index) reject(new Error('Account index out of range'))
-
-			this.currentIndex = index;
-			this.current = this.list[index];
-			resolve()
-		})
-	}
-
-	selectFirst()
-	{
-		return this.select(0);
-	}
-}
-
-
-var accounts = new Accounts();
 accounts.db.on('open',() => {
+	/*
+		Fetch all accounts from database
+	 */
 	accounts.fetchAll().then(rows => {
 		accounts.list = rows;
-	}).then(() => {
+	})
+	/*
+		Select first account from list
+	 */
+	.then(() => {
 		return accounts.selectFirst();
-	}).then(() => {
+	})
+	.then(() => {
 		debug.error(accounts.current)
 	});
 })
